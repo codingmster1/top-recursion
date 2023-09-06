7276996c0773981a4f8a95dd794230167bf090ff
Jeff <doylejeff98@gmail.com>
2023-09-05 23:18:23 -0400

    recursion version of fibonacci

--- Assignment1/fibonacci.js
+++ Assignment1/fibonacci.js
@@ -16,3 +16,16 @@ function fib(n) {
   }
   
   console.log(fib(7)); // added 7 instead of 8 because 8 would count the zero before one and be nine instead
+  // prints Array(8)
+  // 0:0, 1:1, 2:1, 3:2, 4:3, 5:5, 6:8, 7:13 length: 8
+
+
+  //recursion version 
+
+  function fibonacci(n) {
+    return n < 1 ? 0
+         : n <= 2 ? 1
+         : fibonacci(n - 1) + fibonacci(n - 2)
+ }
+ 
+ console.log(fibonacci(7))
